cfe9567661fa989dbd431c2d0d9f6079cf1705b8
tpetersen0308 <travis.e.petersen@gmail.com>
2018-07-13 17:07:01 -0700

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -14,13 +14,30 @@ export default class Tier1 extends Component {
     }
   }
 
+  handleClick = () => {
+    let newColor = getRandomColor();
+    this.setState({
+      color: newColor,
+      childColor: getReducedColor(newColor),
+    })
+  }
+
+  handleChildClick = (event) => {
+    event.stopPropagation();
+    let newColor = getRandomColor();
+    this.setState({
+      ...this.state,
+      childColor: newColor,
+    })
+  }
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
-        <Tier2 color={"#0F0"} />
-        <Tier2 color={"#0FF"} />
+      <div onClick={this.handleClick} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
+        <Tier2 color={this.state.childColor} handleClick={this.handleChildClick}/>
+        <Tier2 color={this.state.childColor} handleClick={this.handleChildClick}/>
       </div>
     )
   }
--- src/Tier2.js
+++ src/Tier2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { getReducedColor } from './randomColorGenerator.js'
+import { getReducedColor, getRandomColor } from './randomColorGenerator.js'
 import Tier3 from './Tier3'
 
 
@@ -12,13 +12,27 @@ export default class Tier2 extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      childColor: getReducedColor(nextProps.color),
+    })
+  }
+
+  handleChildClick = (event) => {
+    event.stopPropagation();
+    let newColor = getRandomColor();
+    this.setState({
+      childColor: getReducedColor(newColor),
+    })
+  }
+
   render() {
     // hard coded color values have been added below, though they won't be
     // present in our solution. What should they be replaced with?
     return (
-      <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
-        <Tier3 color={"#0F0"} />
-        <Tier3 color={"#F00"} />
+      <div onClick={this.props.handleClick} className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick}/>
+        <Tier3 color={this.state.childColor} handleChildClick={this.handleChildClick}/>
       </div>
     )
   }
